a2e4a72652409a28f179a2e61fffa5bd9dd232cc
Christian <christianatencio@upeu.edu.pe>
2021-07-18 17:28:26 -0500

    make it better

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 import { Pool } from 'pg'
 
 export const pool = new Pool({
-    host: 'ec2-23-20-124-77.compute-1.amazonaws.com',
-    user: 'jvcmwinlzoceyl',
-    password: '3da747c7d3b8b81e702c48cd2fe0aa705ae5b388efb6268904df15b3b648769a',
-    database: 'd37n8fm0qs96l6',
+    host: 'ec2-52-1-20-236.compute-1.amazonaws.com',
+    user: 'sbhewhshrysesb',
+    password: 'db397d5ed4e6f98ba64628a993b3b14d42d0c525a7b82546f93cf2d5fdbdd9ed',
+    database: 'dn2cvdp1qni7u',
     port: 5432,
     ssl: { rejectUnauthorized: false }
 })
