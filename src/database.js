eb1f8902d95f2f25890ab6078d052411c1c7d068
Christian <christianatencio@upeu.edu.pe>
2021-07-19 20:28:00 -0500

    final

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 import { Pool } from 'pg'
 
 export const pool = new Pool({
-    host: 'ec2-52-1-20-236.compute-1.amazonaws.com',
-    user: 'sbhewhshrysesb',
-    password: 'db397d5ed4e6f98ba64628a993b3b14d42d0c525a7b82546f93cf2d5fdbdd9ed',
-    database: 'dn2cvdp1qni7u',
+    host: 'ec2-44-194-183-115.compute-1.amazonaws.com',
+    user: 'kysnjhaolhbiod',
+    password: '65a4cf1ff4567bfeceb34cb614e53d7841872ab569123af64216d17840d3a0a3',
+    database: 'diupjjgnsb4gf',
     port: 5432,
     ssl: { rejectUnauthorized: false }
 })
